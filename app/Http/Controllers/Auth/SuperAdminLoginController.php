eccd47e8c6a352361b1090826f6b92b13c089c94
DerThomy <thomy.koe@googlemail.com>
2018-03-13 07:05:09 +0100

    first commit

---
+++ app/Http/Controllers/Auth/SuperAdminLoginController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class SuperAdminLoginController extends Controller
+{
+    public function __construct(){
+        $this->middleware('guest:superadmin');
+    }
+
+    public function showLoginForm(){
+        return view('auth.superadmin-login');
+    }
+
+    public function login(Request $request){
+        // Validate the form data
+        $this->validateLogin($request);
+
+        // Attempt to login
+        if($this->attemptLogin($request)){
+            // if succesfull
+            return $this->sendLoginResponse($request);
+        }
+        
+        // if unsuccessfull
+        return $this->sendFailedLoginResponse();
+    }
+
+
+
+    protected function sendLoginResponse(Request $request){
+        $request->session()->regenerate();
+
+
+        return $this->authenticated($request, $this->guard()->user())
+                ?: redirect()->intended($this->redirectPath());
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        //
+    }
+
+    protected function attemptLogin(Request $request){
+        $this->guard()->attempt($this->credentials($request), $request->filled('remember'));
+    }
+
+    protected function credentials(Request $request){
+        return $request->only($this->username(), 'password');
+    } 
+
+    protected function validateLogin(Request $request){
+        $this->validate($request, [
+            $this->username() => 'required|string',
+            'password' => 'required|string'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard('superadmin')->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
+
+    public function username(){
+        return 'email';
+    }
+
+    public function redirectPath(){
+        return route('superadmin.dashboard');
+    }
+
+    protected function guard(){
+        return Auth::guard('superadmin');
+    }
+}
